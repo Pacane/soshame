d0dc5298337dacb818d3cf52220b64add4b3d2fe
Joel Trottier-Hebert <iamquebecois@gmail.com>
2018-11-07 14:10:44 -0500

    naming

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,6 @@
 import 'package:angular/angular.dart';
 
-import 'src/todo_list/post_list_component.dart';
+import 'src/post_list/post_list_component.dart';
 
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
